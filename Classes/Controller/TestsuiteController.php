298c1c546a6fc899f653472c0a544ffab0c712c6
Marco Huber <marco.huber@bgm-gmbh.de>
2015-09-07 21:16:05 +0200

    Basic backend module

---
+++ Classes/Controller/TestsuiteController.php
@@ -0,0 +1,58 @@
+<?php
+namespace BGM\BgmVrt\Controller;
+
+class TestsuiteController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
+
+	/**
+	 * @var \BGM\BgmVrt\Domain\Repository\TestsuiteRepository
+	 * @inject
+	 */
+	protected $testsuiteRepository;
+
+	/**
+	 * @var \TYPO3\CMS\Extbase\Persistence\PersistenceManagerInterface
+	 * @inject
+	 */
+	protected $persistenceManager;
+
+	/**
+	 * @var \TYPO3\CMS\Core\Log\Logger
+	 */
+	protected $debugLogger;
+
+	/**
+	 * Initialize action
+	 *
+	 * Executed before the requested action is executed.
+	 */
+	public function initializeAction() {
+		if ($this->settings['debug']) {
+			/** @var \TYPO3\CMS\Core\Log\LogManager $loggerManager */
+			$loggerManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Log\\LogManager');
+			$this->debugLogger = $loggerManager->getLogger('syncserver');
+		}
+	}
+
+	/**
+	 * List action
+	 *
+	 * Lists all testsuites.
+	 */
+	public function listAction() {
+		$testsuites = $this->testsuiteRepository->findAll();
+		$this->view->assign('testsuites', $testsuites);
+	}
+
+	/**
+	 * Show action
+	 *
+	 * Shows the details for one testsuite.
+	 *
+	 * @param \BGM\BgmVrt\Domain\Model\Testsuite $testsuite
+	 */
+	public function showAction(\BGM\BgmVrt\Domain\Model\Testsuite $testsuite) {
+		$this->view->assign('testsuite', $testsuite);
+	}
+}
+
+?>
--- ext_tables.php
+++ ext_tables.php
@@ -32,4 +32,25 @@ $GLOBALS['TCA']['tx_bgmvrt_domain_model_testsuite'] = array(
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_bgmvrt_domain_model_testsuite', 'EXT:bgm_vrt/Resources/Private/Language/locallang_csh_tx_bgmvrt_domain_model_testsuite.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_bgmvrt_domain_model_testsuite');
 
+if (TYPO3_MODE === 'BE') {
+
+	/**
+	 * Registers a Backend Module
+	 */
+	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+		'BGM.' . $_EXTKEY,
+		'tools',     // Make module a submodule of 'web'
+		'bgmvrt',    // Submodule key
+		'',          // Position
+		array(
+			'Testsuite' => 'list, show',
+		),
+		array(
+			'access' => 'user,group',
+			'icon' => 'EXT:' . $_EXTKEY . '/ext_icon.gif',
+			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_module.xlf',
+		)
+	);
+}
+
 ?>
